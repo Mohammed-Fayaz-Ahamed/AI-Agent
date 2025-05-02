325cfe559c278e865433a4cb05b9d0138c63efa9
Mohammed-Fayaz-Ahamed <149478883+Mohammed-Fayaz-Ahamed@users.noreply.github.com>
2025-05-02 11:14:40 +0530

    Update model_loader.py

--- ai_agent_app/model_loader.py
+++ ai_agent_app/model_loader.py
@@ -4,7 +4,7 @@ from langchain.llms import HuggingFaceEndpoint
 import streamlit as st
 
 def load_llm():
-    repo_id = "deepseek-ai/DeepSeek-Prover-V2-7B"
+    repo_id = "tiiuae/falcon-7b-instruct"
     return HuggingFaceEndpoint(
         repo_id=repo_id,
         huggingfacehub_api_token=st.secrets["HUGGINGFACE_API_KEY"],
