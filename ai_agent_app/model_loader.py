a35d6679cc58b25003e13adf000ae2589cd8c561
Mohammed-Fayaz-Ahamed <149478883+Mohammed-Fayaz-Ahamed@users.noreply.github.com>
2025-05-02 11:03:25 +0530

    Update model_loader.py

--- ai_agent_app/model_loader.py
+++ ai_agent_app/model_loader.py
@@ -4,7 +4,7 @@ from langchain.llms import HuggingFaceEndpoint
 import streamlit as st
 
 def load_llm():
-    repo_id = "microsoft/Phi-4-mini-reasoning"
+    repo_id = "microsoft/bitnet-b1.58-2B-4T"
     return HuggingFaceEndpoint(
         repo_id=repo_id,
         huggingfacehub_api_token=st.secrets["HUGGINGFACE_API_KEY"],
