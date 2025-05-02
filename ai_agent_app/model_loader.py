9c3c478992ea06e4852b7045b84f20fff72b90b9
Mohammed-Fayaz-Ahamed <149478883+Mohammed-Fayaz-Ahamed@users.noreply.github.com>
2025-05-02 11:18:15 +0530

    Update model_loader.py

--- ai_agent_app/model_loader.py
+++ ai_agent_app/model_loader.py
@@ -9,6 +9,6 @@ def load_llm():
         repo_id=repo_id,
         huggingfacehub_api_token=st.secrets["HUGGINGFACE_API_KEY"],
         task="text-generation",
-        temperature=0.7,
+        temperature=0.4,
         max_new_tokens=2000
     )
