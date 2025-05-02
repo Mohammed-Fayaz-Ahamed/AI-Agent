ed2ee76d608b86575866898fd910c98c87472c43
Mohammed-Fayaz-Ahamed <149478883+Mohammed-Fayaz-Ahamed@users.noreply.github.com>
2025-05-02 11:16:17 +0530

    Update model_loader.py

--- ai_agent_app/model_loader.py
+++ ai_agent_app/model_loader.py
@@ -4,7 +4,7 @@ from langchain.llms import HuggingFaceEndpoint
 import streamlit as st
 
 def load_llm():
-    repo_id = "tiiuae/falcon-7b-instruct"
+    repo_id = "HuggingFaceH4/zephyr-7b-beta"
     return HuggingFaceEndpoint(
         repo_id=repo_id,
         huggingfacehub_api_token=st.secrets["HUGGINGFACE_API_KEY"],
