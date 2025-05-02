0b4c92318c308924575e3950b20518449256df2b
Mohammed-Fayaz-Ahamed <149478883+Mohammed-Fayaz-Ahamed@users.noreply.github.com>
2025-05-02 11:08:25 +0530

    Update model_loader.py

--- ai_agent_app/model_loader.py
+++ ai_agent_app/model_loader.py
@@ -4,7 +4,7 @@ from langchain.llms import HuggingFaceEndpoint
 import streamlit as st
 
 def load_llm():
-    repo_id = "Qwen/Qwen3-235B-A22B"
+    repo_id = "qwen/qwen3-235B-A22B"
     return HuggingFaceEndpoint(
         repo_id=repo_id,
         huggingfacehub_api_token=st.secrets["HUGGINGFACE_API_KEY"],
